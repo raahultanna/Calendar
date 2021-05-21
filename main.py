bec574a0765a5deececa74456445670714be18ad
Captain Nemo <rahul.tanna94@gmail.com>
2021-05-21 20:04:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# Calendar
+
+import calendar
+
+# Enter the month and year
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(year, month))
